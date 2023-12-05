fd727e938d640ef143c177ebd46424b26a5d9a08
James Vickers <jamesvickers@jamess-imac.home>
2023-12-05 13:17:03 +0000

    FEAT: query getAllTasks endpoint using axios and render to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,49 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8888/tasks/get/all')
+      .then((res) => {
+        console.log('res: ', res)
+        const { tasks: axiosTasks } = res.data
+        setTasks(axiosTasks);
+        setIsLoading(false);
+      })
+  }, [])
+
   return (
     <main>
       <h1>
         Task Manager Frontend
       </h1>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : (
+        <div>
+          <h2>Task count: {tasks.length}</h2>
+          {tasks && tasks.map((task) => {
+            const { _id: id, assignee, description, priority } = task;
+            return (
+              <>
+
+                <ul>
+                  <li>{id}</li>
+                  <li>{assignee}</li>
+                  <li>{description}</li>
+                  <li>{priority}</li>
+                </ul>
+              </>
+            )
+          })}
+        </div>
+      )
+      }
     </main>
   )
 }
