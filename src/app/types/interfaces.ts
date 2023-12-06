84d871743c03c4ad11ff4381f3549dd4d3d285cb
James Vickers <jamesvickers@jamess-imac.home>
2023-12-06 10:28:01 +0000

    FIX: add NewTask interface to distinguish from Task interface with _id added on server

--- src/app/types/interfaces.ts
+++ src/app/types/interfaces.ts
@@ -1,7 +1,14 @@
 import { Priority } from './types';
 
 export interface ITask extends Document {
-    _id?: string; // Optional as new tasks do not yet have an _id. Unique _id is generated on the server. 
+    _id: string;
+    assignee: string;
+    description: string;
+    priority: Priority;
+}
+
+// New tasks do not yet have an _id. Unique _id is generated on the server. 
+export interface INewTask extends Document {
     assignee: string;
     description: string;
     priority: Priority;
